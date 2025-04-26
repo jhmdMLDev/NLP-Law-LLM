e7064b9bb45f89ecf2720bcc4fcc83103499df68
Javadhmd <javadhamidies1@gmail.com>
2025-04-25 19:19:28 -0600

    Make the frontend to work

---
+++ Frontend/server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+http.createServer((req, res) => {
+  if (req.url === '/') {
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, 'utf-8', (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading file');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end('Not found');
+  }
+}).listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
